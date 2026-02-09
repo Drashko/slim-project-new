701907963dff17393264a77486b15b7468090372
Drashko <drashko1979@gmail.com>
2026-02-09 16:11:23 +0200

    Add permission management API and admin UI

--- config/routes.php
+++ config/routes.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use App\API\Endpoint\V1\Admin\HomeAdminEndpoint;
+use App\API\Endpoint\V1\Admin\PermissionEndpoint;
 use App\API\Endpoint\V1\Public\HomeEndpoint;
 use App\Integration\Middleware\CasbinAuthorizationMiddleware;
 use Psr\Http\Message\ServerRequestInterface;
@@ -17,13 +18,15 @@ return static function (App $app): void {
         return $response
             ->withHeader('Access-Control-Allow-Origin', '*')
             ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
-            ->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+            ->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Subject, X-Scope');
     });
 
     $app->group('/api', function (RouteCollectorProxy $group): void {
         $group->group('/v1', function (RouteCollectorProxy $versionGroup): void {
             $versionGroup->get('', [HomeEndpoint::class, 'index'])->setName('api.v1.home');
             $versionGroup->get('/admin', [HomeAdminEndpoint::class, 'index'])->setName('api.v1.admin.home');
+            $versionGroup->get('/admin/permissions', [PermissionEndpoint::class, 'index'])->setName('api.v1.admin.permissions');
+            $versionGroup->post('/admin/permissions', [PermissionEndpoint::class, 'create'])->setName('api.v1.admin.permissions.create');
         })->add(CasbinAuthorizationMiddleware::class);
     });
 };
