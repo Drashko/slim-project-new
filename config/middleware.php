36c76c61df4fd868bfec5330ecae8b10b862d63c
Drashko <drashko1979@gmail.com>
2026-02-10 13:57:04 +0200

    fix cors headers for error responses via global middleware

--- config/middleware.php
+++ config/middleware.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
 use App\Integration\Middleware\StaticAssetCacheMiddleware;
 use Slim\App;
 use Slim\Middleware\ErrorMiddleware;
@@ -11,4 +13,13 @@ return static function (App $app): void {
     $app->addRoutingMiddleware();
     $app->add(StaticAssetCacheMiddleware::class);
     $app->add(ErrorMiddleware::class);
+
+    $app->add(function (ServerRequestInterface $request, RequestHandlerInterface $handler) {
+        $response = $handler->handle($request);
+
+        return $response
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
+            ->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Subject, X-Scope');
+    });
 };
--- config/routes.php
+++ config/routes.php
@@ -3,11 +3,9 @@
 declare(strict_types=1);
 
 use App\API\Endpoint\V1\Admin\HomeAdminEndpoint;
-use App\API\Endpoint\V1\Admin\PermissionEndpoint;
 use App\API\Endpoint\V1\Public\HomeEndpoint;
 use App\Integration\Middleware\CasbinAuthorizationMiddleware;
 use Psr\Http\Message\ServerRequestInterface;
-use Psr\Http\Server\RequestHandlerInterface;
 use Psr\Http\Message\ResponseInterface;
 use Slim\App;
 use Slim\Routing\RouteCollectorProxy;
@@ -20,21 +18,10 @@ return static function (App $app): void {
             ->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Subject, X-Scope');
     });
 
-    $app->add(function (ServerRequestInterface $request, RequestHandlerInterface $handler) {
-        $response = $handler->handle($request);
-
-        return $response
-            ->withHeader('Access-Control-Allow-Origin', '*')
-            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
-            ->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Subject, X-Scope');
-    });
-
     $app->group('/api', function (RouteCollectorProxy $group): void {
         $group->group('/v1', function (RouteCollectorProxy $versionGroup): void {
             $versionGroup->get('', [HomeEndpoint::class, 'index'])->setName('api.v1.home');
             $versionGroup->get('/admin', [HomeAdminEndpoint::class, 'index'])->setName('api.v1.admin.home');
-            $versionGroup->get('/admin/permissions', [PermissionEndpoint::class, 'index'])->setName('api.v1.admin.permissions');
-            $versionGroup->post('/admin/permissions', [PermissionEndpoint::class, 'create'])->setName('api.v1.admin.permissions.create');
         })->add(CasbinAuthorizationMiddleware::class);
     });
 };
